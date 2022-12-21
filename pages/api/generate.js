f4bb9c5f9ebbf33be7cd080d0c1ed05c865aba6e
Jakob Evangelista <jakobevangelista@tamu.edu>
2022-12-21 00:37:33 -0600

    FIXED OPEN AI ENV STUFF

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,4 +1,4 @@
-import { Configuration, OpenAIApi } from "openai";
+import { Configuration, OpenAIApi } from 'openai';
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -7,7 +7,7 @@ const openai = new OpenAIApi(configuration);
 
 export default async function (req, res) {
   const completion = await openai.createCompletion({
-    model: "text-davinci-002",
+    model: 'text-davinci-003',
     prompt: generatePrompt(req.body.animal),
     temperature: 0.6,
   });
